d71f7a82a96ac455a08b7945ef22a202ba62cea5
ramonsnchz <41165971+ramonsnchz@users.noreply.github.com>
2019-04-14 17:34:11 -0500

    Add files via upload

--- Github/doc2vec.py
+++ Github/doc2vec.py
@@ -1,5 +1,6 @@
-
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.lancaster import LancasterStemmer
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -14,6 +15,14 @@ from nltk.tokenize import word_tokenize
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+# word stemmer
+stemmer = LancasterStemmer()
+def preprocess(dataframe,c):
+    addtl_stopwords = []
+    stop = stopwords.words('english')+addtl_stopwords
+    dataframe[c] = map(lambda x: ' '.join([stemmer.stem(word) for word in x.split() if word not in stop]), dataframe[c])
+    #dataframe[c] = map(lambda x: ' '.join([word for word in x.split() if word not in stop]), dataframe[c])
+    return dataframe[c]
 
 def chunks(l, n):
     for i in range(0, len(l), n):
@@ -22,69 +31,78 @@ def chunks(l, n):
         else:
             vector += [l[i:i + n]]
     return vector
+accVector = [0]*10
+for ac in range(15):
+    
+	#number of words to split document into
+	n = 4
 
-#number of words to split document into
-n = 10
+	dataframe = pd.read_csv('training_data_w_labels.csv',  error_bad_lines=False)
+	dataframe['question'] = map(lambda x: x.lower(), dataframe['question'])
+	dataframe['question'] = map(lambda x: x.rstrip('?'), dataframe['question'])
+	#print(preprocess(dataframe,'question'))
+	training_data = np.load('training_data.npy')
+	training_values = [" ".join(training_data[i].values()) for i in range(training_data.size)]
+	tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags = [str(i)]) for i,_d in enumerate(training_values)]
+	#print(tagged_data)
+	bow = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	for i in range(len(tagged_data)):
+	    #split into 10 words
+	    bow[i] = chunks(tagged_data[i][0],n)
 
-dataframe = pd.read_csv('training_data_w_labels.csv')
+	sections = dataframe.section.unique()
+	df = pd.DataFrame([])
+	for i in range(0,len(training_data)):
+	    dfnew = pd.DataFrame({'sections':sections[i],'words':bow[i]})
+	    #print(dfnew)
+	    df = df.append(dfnew, ignore_index = True)
+	df['words'] = [' '.join(map(str,l)) for l in df['words']]
+	#print(df)
 
-training_data = np.load('training_data.npy')
-training_values = [" ".join(training_data[i].values()) for i in range(training_data.size)]
-tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags = [str(i)]) for i,_d in enumerate(training_values)]
+	def label_sentences(corpus, label_type):
+	    labeled = []
+	    for i, v in enumerate(corpus):
+		label = label_type + '_' + str(i)
+		labeled.append(TaggedDocument(v.split(), [label]))
+	    return labeled
 
-bow = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-for i in range(len(tagged_data)):
-    #split into 10 words
-    bow[i] = chunks(tagged_data[i][0],n)
+	#X_train = df.words
+	X_train = preprocess(df,'words')
+	y_train =df.sections
+	#X_test = dataframe.question
+	X_test = preprocess(dataframe,'question')
+	y_test = dataframe.section 
 
-sections = ['VERTEX CARE AND HANDLING', 'GENERAL', 'NOTES ON SAFETY', 'TECHNICAL DATA', 'TRANSPORT/DELIVERY/STORAGE', 'INSTALLATION AND REMOVAL', 'COMMISIONING', 'UTILIZING THE HART COMMUNICATION', 'MAINTENANCE','TROUBLESHOOTING','DYNISCO CONTACT INFOMRATION','ACCESSORIES','APPROVALS/CERTIFICATES', 'OUTLINE DRAWINGS']
+	X_test = label_sentences(X_test, 'Test')
+	X_train = label_sentences(X_train, 'Train')
 
-df = pd.DataFrame([])
-for i in range(0,len(training_data)):
-    dfnew = pd.DataFrame({'sections':sections[i],'words':bow[i]})
-    #print(dfnew)
-    df = df.append(dfnew, ignore_index = True)
-print(df)
-
-def label_sentences(corpus, label_type):
-    labeled = []
-    for i, v in enumerate(corpus):
-        label = label_type + '_' + str(i)
-        labeled.append(v)
-        #labeled.append(TaggedDocument(v.split(), [label]))
-    return labeled
-
-X_train, X_test, y_train, y_test = train_test_split(df.words, df.sections, random_state=0, train_size=0.7)
-X_train = label_sentences(X_train, 'Train')
-X_test = label_sentences(X_test, 'Test')
-#print(y_train)
-#print(y_test)
-all_data = X_train + X_test
-#print(all_data)
-
-
-model_dbow = Doc2Vec(dm=0, vector_size=300, negative=5, min_count=1, alpha=0.065, min_alpha=0.065)
-model_dbow.build_vocab([x for x in tqdm(all_data)])
-
-for epoch in range(30):
-    model_dbow.train(utils.shuffle([x for x in tqdm(all_data)]), total_examples=len(all_data), epochs=1)
-    model_dbow.alpha -= 0.002
-    model_dbow.min_alpha = model_dbow.alpha
-
-def get_vectors(model, corpus_size, vectors_size, vectors_type):
-    vectors = np.zeros((corpus_size, vectors_size))
-    for i in range(0, corpus_size):
-        prefix = vectors_type + '_' + str(i)
-        vectors[i] = model.docvecs[prefix]
-    return vectors
-
-train_vectors_dbow = get_vectors(model_dbow, len(X_train), 300, 'Train')
-test_vectors_dbow = get_vectors(model_dbow, len(X_test), 300, 'Test')
+	all_data = X_train + X_test
 
 
-logreg = LogisticRegression(n_jobs=1, C=1e5)
-logreg.fit(train_vectors_dbow, y_train)
-logreg = logreg.fit(train_vectors_dbow, y_train)
-y_pred = logreg.predict(test_vectors_dbow)
-print('accuracy %s' % accuracy_score(y_pred, y_test))
-#print(classification_report(y_test, y_pred,target_names=sections))
+	model_dbow = Doc2Vec(dm=0, vector_size=350, negative=5, min_count=1, alpha=0.085, min_alpha=0.085)
+	model_dbow.build_vocab([x for x in tqdm(all_data)])
+
+	for epoch in range(30):
+	    model_dbow.train(utils.shuffle([x for x in tqdm(all_data)]), total_examples=len(all_data), epochs=1)
+	    model_dbow.alpha -= 0.002
+	    model_dbow.min_alpha = model_dbow.alpha
+
+	def get_vectors(model, corpus_size, vectors_size, vectors_type):
+	    vectors = np.zeros((corpus_size, vectors_size))
+	    for i in range(0, corpus_size):
+		prefix = vectors_type + '_' + str(i)
+		vectors[i] = model.docvecs[prefix]
+	    return vectors
+
+	train_vectors_dbow = get_vectors(model_dbow, len(X_train), 350, 'Train')
+	test_vectors_dbow = get_vectors(model_dbow, len(X_test), 350, 'Test')
+
+
+	logreg = LogisticRegression(n_jobs=1, C=1e5)
+	logreg.fit(train_vectors_dbow, y_train)
+	logreg = logreg.fit(train_vectors_dbow, y_train)
+	y_pred = logreg.predict(test_vectors_dbow)
+	print('accuracy %s' % accuracy_score(y_pred, y_test))
+	#print(classification_report(y_test, y_pred,target_names=sections))
+        accVector[ac] = accuracy_score(y_pred, y_test)
+print((sum(accVector)/15))
